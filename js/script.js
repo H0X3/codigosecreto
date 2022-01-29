c036edd6bfd70a4e3701625e04376b479e581772
H0X3 <83131627+H0X3@users.noreply.github.com>
2022-01-28 22:03:30 -0500

    Actualizacion del codigo

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@ const cifrado = {'e': 'enter','i': 'imes','a': 'ai','o': 'ober','u': 'ufat',};
 
 // Funcion de Encriptar
 function encriptar(){
-let letras = document.getElementById('mensaje').value;
-letras = normalizar(letras);
-for(key in cifrado){ 
-    letras = letras.replaceAll(key, cifrado[key]);
-}
+  sonido();
+  let letras = document.getElementById('mensaje').value;
+  letras = normalizar(letras);
+  for(key in cifrado){ 
+      letras = letras.replaceAll(key, cifrado[key]);
+  }
+
 document.getElementById("resultado").value = letras;
+
 }
 
 // Funcion de Desencriptar 
 function desencriptar(){
+  sonido();
   let letras = document.getElementById('mensaje').value;
   letras = normalizar(letras);
   for(key in cifrado){
@@ -23,25 +27,28 @@ function desencriptar(){
 
 // Funcion Copiar
 function copiar(){
+  sonido();
   let contenido = document.getElementById('resultado');
   contenido.select();
   document.execCommand('copy');
-  document.getElementById('mensaje').focus();
+  // document.getElementById('mensaje').focus();
 }
 
 // Funcion Pegar
 function pegar(){
+  sonido();
   let contenido = document.getElementById('resultado').value;
   document.getElementById("mensaje").value = contenido;
   borrar();
-  document.getElementById('mensaje').focus();
+  //document.getElementById('mensaje').focus();
 
 }
 
 // Funcion Borrar
 function borrar(caja){
+  sonido();
   if (caja == "arriba"){
-  document.getElementById("mensaje").value = "";
+    document.getElementById("mensaje").value = "";
   }else{
     document.getElementById("resultado").value = "";
   }
@@ -51,4 +58,17 @@ function borrar(caja){
 // Funcion Normalizar Texto
 function normalizar(texto) {
   return texto.normalize('NFD').replace(/([aeiou])[\u0300-\u0308]/gi,"$1").normalize().toLowerCase();
+}
+
+function sonido(){
+
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "sound/sonido_uno.mp3");
+    etiquetaAudio.play();
+}
+
+function play(){
+  var imagen = document.getElementById("imagen");
+  imagen.style.backgroundImage = "url('tuUrl')";
+
 }
